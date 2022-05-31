d81b641de3abfd37f092b8621fb97fe6c96c80fa
Manish Vachharajani <manishv@unbounded.systems>
2022-05-31 14:58:02 -0600

    feat: Shorten banner text, add sample To Do items

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -2,11 +2,28 @@
  * REST endpoint for /todos
  */
 
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, PrismaClientKnownRequestError } = require('@prisma/client');
 const express = require('express');
 const router = express.Router();
 
 const prisma = new PrismaClient();
+
+// REMOVE TODO ITEMS BEGIN 
+const prepop = [
+  { id: "feedfacefeedfacefeedface", title: '<a href="http://adaptable.io/docs/starters/express-prisma-mongo-starter#idea-2-deploy-a-code-update">Deploy a code update</a> by removing the banner message', done: false },
+  { id: "beeffeedbeeffeedbeeffeed", title: '<a href="https://adaptable.io/docs/starters/express-prisma-mongo-starter#idea-3-start-building-your-app-by-adding-more-api-services">Customize this app</a> by adding an API service to delete To Do items', done: false },
+];
+
+prepop.map((i) => prisma.TodoItem.create({ data: i })
+  .then(() => console.log(`Added pre-populated item with id ${i.id}`))
+  .catch((e) => {
+    if(!((e instanceof PrismaClientKnownRequestError)
+      && e.code === "P2002")) {
+      console.error(`Error creating prepopulated item ${i.id}: ${e.message}`);
+    } // else prepopulated entries are already present
+  }
+));
+// REMOVE TODO ITEMS END
 
 const asyncMiddleware = fn => (req, res, next) => {
   Promise.resolve(fn(req, res, next))
